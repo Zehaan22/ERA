3af36377975d3662bab66fa34f57fb288e90239c
Zehaan <zehaan22@iitk.ac.in>
2023-08-05 22:05:36 +0530

    bot location testing

--- Arm/Arucomasti/detection.py
+++ Arm/Arucomasti/detection.py
@@ -2,6 +2,40 @@ import cv2 as cv
 from cv2 import aruco
 import numpy as np
 
+list1 = []  # upper
+list2 = []  # left
+list3 = []  # right
+list4 = []  # lower
+id_to_cordinate = {
+    1: [1, 2],
+    2: [3, 4]
+
+}
+
+
+def locate_bot(ids, distance, theta):
+    a = id_to_cordinate[ids]
+    x1 = a[0]
+    y1 = a[1]
+    if ids in list1:
+        xf = x1-distance*np.sin(theta)
+        yf = y1-distance*np.cos(theta)
+
+    if ids in list2:
+        xf = distance*np.cos(theta)
+        yf = y1-distance*np.sin(theta)
+
+    if ids in list3:
+        xf = x1-distance*np.cos(theta)
+        yf = y1-distance*np.sin(theta)
+
+    if ids in list4:
+        xf = x1-distance*np.sin(theta)
+        yf = distance*np.cos(theta)
+
+    return xf, yf
+
+
 calib_data_path = "../calib_data/MultiMatrix.npz"
 
 calib_data = np.load(calib_data_path)
@@ -30,15 +64,16 @@ while True:
     gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     markerCorners, markerIds, rejectedCandidates = detector.detectMarkers(
         gray_frame)
-    
+
     if markerCorners:
-        rVec, tVec, _ = cv.aruco.estimatePoseSingleMarkers(
+        rVec, tVec, _ = aruco.estimatePoseSingleMarkers(
             markerCorners, MARKER_SIZE, cam_mat, dist_coef
         )
         total_markers = range(0, markerIds.size)
         for ids, corners, i in zip(markerIds, markerCorners, total_markers):
             cv.polylines(
-                frame, [corners.astype(np.int32)], True, (0, 255, 255), 4, cv.LINE_AA
+                frame, [corners.astype(np.int32)], True, (0,
+                                                          255, 255), 4, cv.LINE_AA
             )
             corners = corners.reshape(4, 2)
             corners = corners.astype(int)
@@ -53,8 +88,24 @@ while True:
             distance = np.sqrt(
                 tVec[i][0][2] ** 2 + tVec[i][0][0] ** 2 + tVec[i][0][1] ** 2
             )
+
+            sa = abs(top_right[0] - top_left[0]) + \
+                abs(bottom_left[0] - bottom_right[0])
+            si_right = abs(top_right[1] - bottom_right[1])
+            si_left = abs(top_left[1] - bottom_left[1])
+
+            theta_z = np.arccos(sa/si_right)
+
+            if si_right < si_left:
+                theta_z *= -1
+            else:
+                theta_z *= 1
+
+            location = locate_bot(ids, distance, theta_z)
+
             # Draw the pose of the marker
-            point = cv.drawFrameAxes(frame, cam_mat, dist_coef, rVec[i], tVec[i], 4, 4)
+            point = cv.drawFrameAxes(
+                frame, cam_mat, dist_coef, rVec[i], tVec[i], 4, 4)
             cv.putText(
                 frame,
                 f"id: {ids[0]} Dist: {round(distance, 2)}",
@@ -76,7 +127,7 @@ while True:
                 cv.LINE_AA,
             )
 
-    cv.imshow("frame",frame)
+    cv.imshow("frame", frame)
     key = cv.waitKey(1)
     if key == ord('q'):
         break
